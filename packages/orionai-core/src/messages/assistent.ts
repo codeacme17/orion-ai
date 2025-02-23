856798cb6fcb7bd4c11280c6db5d9b5fccce9199
leyoonafr <codeacme17@gmail.com>
2025-02-23 16:20:20 +0800

    feat(core): add AssistantMessage class for assistant role handling; update user message to include role property

---
+++ packages/orionai-core/src/messages/assistent.ts
@@ -0,0 +1,12 @@
+import { BaseMessage, type IBaseMessageFields, type TMessageType } from "./base";
+
+interface IAssistantMessageFiels extends IBaseMessageFields {}
+
+export class AssistantMessage extends BaseMessage {
+  role: TMessageType;
+
+  constructor(fields: IAssistantMessageFiels | string) {
+    super(fields);
+    this.role = "assistant";
+  }
+}
